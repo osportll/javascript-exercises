1f620361c2be8b8ec4b4945584378013f3722828
Oscar <oscarportillo721@gmail.com>
2022-04-06 21:17:56 -0300

    Added solutions for exer 02, 03, 04 and 05

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,25 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+
+    let repeatedString = string;
+
+    if (num === 0) {
+        return "";
+        } else if (num < 0) {
+            return "ERROR";
+        } else {
+            for (let i = 1; i < num; i++) {
+    
+                repeatedString += string;
+             
+             }
+        }
+
+console.log(repeatedString);
+return repeatedString;
+
 
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,37 @@
-const sumAll = function() {
+const sumAll = function(...args) {
 
+    let newArray = [];
+    let argArray = Array.from(args);
+
+    let max = Math.max(...argArray);
+    let min = Math.min(...argArray);
+
+    for (i = min; i <= max; i++) {
+
+        if (!args.includes(i)) {
+            newArray.push(i);
+        }
+    }
+
+    newArray.push(argArray[1]); //add an element to the end of newArray
+    newArray.unshift(argArray[0]); //add element to the beggining
+
+    let total = newArray.reduce(function(a, b){return a + b;})
+
+    console.log(total);
+
+    let result = args.filter(arg => typeof arg === 'number');
+    let result2 = args.find(arg => arg < 0);
+
+    if (result.length === 1) {
+        return 'ERROR';
+    } else if (result2 < 0) {
+        return 'ERROR';
+    } else return total;
 };
+
+sumAll(1, 4);
+
 
 // Do not edit below this line
 module.exports = sumAll;
