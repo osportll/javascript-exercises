60021f3bd25b4f2cc1a049a9b3411e8c64e2c04b
Oscar <oscarportillo721@gmail.com>
2022-04-09 14:07:29 -0300

    Exercise 7 solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,20 @@
-const ftoc = function() {
+const ftoc = function(fahrenheit) {
+
+  let result = (fahrenheit - 32) * (5 / 9);
+
+  let rounded = Math.round(result * 10) / 10;
+
+  return rounded;
 
 };
 
-const ctof = function() {
+const ctof = function(celcius) {
+
+  let result = ((celcius * (9/5)) + 32);
+
+  let rounded = Math.round(result * 10) / 10;
+
+  return rounded;
 
 };
 
