c19a00309c8cd0001e050ec056ab10bd3d228b00
Oscar <oscarportillo721@gmail.com>
2022-04-09 00:39:40 -0300

    Exercise 6 solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,27 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    if (year % 100 === 0 && year % 400 === 0 && year % 4 === 0) {
+
+        return true;
+
+    } else if (year % 100 === 0) {
+
+        return false;
+
+    } else if (year % 4 === 0 || year % 400 === 0) {
+        return true;
+
+    } else {
+        return false;
+    }
 };
+
+let result = leapYears(2000);
+
+console.log(result);
+
+console.log(1600 % 100, 1600 % 400, 1600 % 4);
+console.log(700 % 100, 700 % 400, 700 % 4);
 
 // Do not edit below this line
 module.exports = leapYears;
